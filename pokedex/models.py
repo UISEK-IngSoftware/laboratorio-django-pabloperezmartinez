1237d0250a45d1d97d15b317aeccff8b72e2f1af
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2025-05-21 18:20:39 -0500

    Laboratorio 5. Taller Inicial de Django: Views, Templates y Modelos

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=40, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.IntegerField(null=False)
+    height = models.IntegerField(null=False)
+    
+    def __str__(self):
+        return self.name
