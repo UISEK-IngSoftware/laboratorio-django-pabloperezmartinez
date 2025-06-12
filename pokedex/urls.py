c24c3a47fde7d5f691572d305de50456cef459f1
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2025-06-12 16:34:09 -0500

    Laboratorio 7 terminado

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("pokemon/<int:id>/", views.pokemon, name="pokemon"),
     path("pokemon/add/", views.add_pokemon, name="add_pokemon"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("edit_pokemon/<int:id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("delete_pokemon/<int:id>/", views.delete_pokemon, name="delete_pokemon"),
 ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render
 from django.template import loader
 from .models import Pokemon
 from .forms import PokemonForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     pokemons = Pokemon.objects.order_by('name')
@@ -17,6 +19,7 @@ def pokemon(request, id: int):
     }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def add_pokemon(request):
     if request.method == 'POST':
         form = PokemonForm(request.POST, request.FILES)
@@ -28,3 +31,25 @@ def add_pokemon(request):
         form = PokemonForm()
             
     return render (request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def edit_pokemon(request, id: int):
+    pokemon = Pokemon.objects.get(pk=id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+        
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render (request, 'pokemon_form.html', {'form': form})
+
+@login_required
+def delete_pokemon(request, id: int):
+    pokemon = Pokemon.objects.get(pk=id)
+    pokemon.delete()
+    return redirect('pokedex:index')
+
+class CustomLoginView(LoginView):
+    template_name = "login_form.html"
